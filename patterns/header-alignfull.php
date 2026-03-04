e226ee2d9c0a2e1604e57f0f492c06ae46cc5678
unknown <ju.velling@gmail.com>
2026-03-04 23:08:40 +0100

    push

--- patterns/header-alignfull.php
+++ patterns/header-alignfull.php
@@ -11,21 +11,21 @@
 
 <!-- wp:group {
  "align":"full",
+ "backgroundColor":"flyhigh-white",
  "layout":{"type":"constrained"},
  "lock":{"move":true,"remove":true},
  "fontSize":"medium",
  "style":{"elements":{"link":{"color":{"text":"var:preset|color|flyhigh-blue"}}}},
  "textColor":"flyhigh-blue",
  "allowedBlocks":[]} -->
-<div class="wp-block-group alignfull has-medium-font-size has-flyhigh-blue-color has-text-color">
+<div class="wp-block-group alignfull has-flyhigh-white-background-color has-background has-medium-font-size has-flyhigh-blue-color has-text-color">
 
     <!-- wp:group {
-     "backgroundColor":"flyhigh-white",
      "layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},
      "lock":{"move":true,"remove":true},
      "style":{"spacing":{"margin":{"right":"auto","left":"auto"},"padding":{"top":"var:preset|spacing|flyhigh-l","bottom":"var:preset|spacing|flyhigh-s"}}},
      "allowedBlocks":["core/paragraph","core/heading","core/buttons","core/group","core/math","core/pullquote","core/separator","core/spacer","core/quote","core/social-links","core/search","core/rss","core/tag-cloud","core/navigation","core/site-logo","core/site-title","core/site-tagline","core/loginout"]} -->
-    <div class="wp-block-group has-flyhigh-white-background-color has-background" style="margin-right:auto;margin-left:auto;padding-top:var(--wp--preset--spacing--flyhigh-l);padding-bottom:var(--wp--preset--spacing--flyhigh-s)">
+    <div class="wp-block-group" style="margin-right:auto;margin-left:auto;padding-top:var(--wp--preset--spacing--flyhigh-l);padding-bottom:var(--wp--preset--spacing--flyhigh-s)">
 
         <!-- wp:site-title {"level":0} /-->
 
